01616df4ceef8e5b047fc816244b9f89ae5b6910
Nadezhda Ilina <nadiailina9@gmail.com>
2023-09-02 20:19:27 +0300

    classes

--- script.js
+++ script.js
@@ -8,7 +8,41 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputTemp = document.querySelector('.form__input--temp');
 const inputClimb = document.querySelector('.form__input--climb');
 
-let map, mapEvent;
+class Workout {
+  date = new Date();
+  id = (Date.now() + '').slice(-10);
+
+  constructor(coords, distance, duration) {
+    this.coords = coords;
+    this.distance = distance; //km
+    this.duration = duration; //min
+  }
+}
+
+class Running extends Workout {
+  constructor(coords, distance, duration, temp) {
+    super(coords, distance, duration);
+    this.temp = temp;
+    this.calculatePace();
+  }
+
+  //min/km
+  calculatePace() {
+    this.pace = this.duration / this.distance;
+  }
+}
+
+class Cycling extends Workout {
+  constructor(coords, distance, duration, climb) {
+    super(coords, distance, duration);
+    this.climb = climb;
+    this.calculateSpeed();
+  }
+  //km/h
+  calculateSpeed() {
+    this.speed = this.distance / this.duration / 60;
+  }
+}
 
 class App {
   #map;
@@ -65,15 +99,43 @@ class App {
   }
 
   _newWorkout(e) {
-    e.preventDefault();
-    //Очистка полей ввода данных
-    inputDistance.value =
-      inputDuration.value =
-      inputTemp.value =
-      inputClimb.value =
-        '';
+    const areNumbers = (...numbers) =>
+      numbers.every(num => Number.isFinite(num));
 
-    //Отображение маркера
+    const areNumbersPositive = (...numbers) => numbers.every(num => num > 0);
+
+    e.preventDefault();
+
+    //Получить данные из формы
+    const type = inputType.value;
+    const distance = +inputDistance.value;
+    const duration = +inputDuration.value;
+
+    //Если пробежка = Running
+    if (type === 'running') {
+      const temp = +inputTemp.value;
+      //Проверка валидности данных
+      if (
+        !areNumbers(distance, duration, temp) ||
+        !areNumbersPositive(distance, duration, temp)
+      )
+        return alert('Введите положительное число!');
+    }
+
+    //Если велотренировка = Cycling
+    if (type === 'cycling') {
+      const climb = +inputClimb.value;
+      //Проверка валидности данных
+      if (
+        !areNumbers(distance, duration, climb) ||
+        !areNumbersPositive(distance, duration)
+      )
+        return alert('Введите положительное число!');
+    }
+
+    //Добавить новый объект в массив тренировок
+
+    //Отобразить тренировку на карте
     const { lat, lng } = this.#mapEvent.latlng;
     L.marker([lat, lng])
       .addTo(this.#map)
@@ -88,7 +150,16 @@ class App {
       )
       .setPopupContent('Тренировка')
       .openPopup();
+    //Отобразить тренировку в списке
+
+    //Спрятать форму и очистить поля ввода данных
+
+    //Очистка полей ввода данных
+    inputDistance.value =
+      inputDuration.value =
+      inputTemp.value =
+      inputClimb.value =
+        '';
   }
 }
-
 const app = new App();
